e8064fdbdeb25c8c546a8058574918b0452a1d9c
Mili <milisharma.noida@gmail.com>
2025-08-20 11:42:04 -0700

    Entity files

---
+++ springboot/src/main/java/com/example/springboot/entity/Movie.java
@@ -0,0 +1,31 @@
+package com.example.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "movies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String director;
+
+    private String genre;
+
+    @Column(nullable = false)
+    private String releaseDate;
+}
---
+++ springboot/src/main/java/com/example/springboot/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+}
